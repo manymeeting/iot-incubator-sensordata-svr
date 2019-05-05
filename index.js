3ffe731d188114294469fe60e6041d33a3ddd40a
Mutian Wang <mokuteno@gmail.com>
2019-05-04 18:16:28 -0700

    Add controllers and memstorage

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+// Memory storage
+const memStorage = {
+	sensorData: {
+		ntc: "23.2",
+		dht: "12.3"
+	},
+	targetTemperature: "",
+	isMotionDetected: false,
+	cameraImgPath: "https://media.chevrolet.com/content/Pages/news/us/en/2017/nov/1112-corvette/_jcr_content/top_parsys/image_1613279304.img.jpg/1510780124493.jpg"
+};
 
 
 // UDP Server
 const PORT = 6868;
-const HOST = "127.0.0.1";
+const HOST = "10.50.0.127";
 
 var dgram = require('dgram');
 var udpServer = dgram.createSocket('udp4');
@@ -18,6 +28,14 @@ udpServer.on('listening', function () {
 
 udpServer.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
+    // Parse message
+    message = message.toString();
+    if(message === "Test") {
+
+    	udpServer.send(Buffer.from('Test reply from server'), remote.port, remote.address, (err) => {
+    		console.log(err);
+    	});
+    }
 });
 
 udpServer.bind(PORT, HOST);
@@ -46,18 +64,38 @@ app.get("/test", (req, res, next) => {
 	res.jsonp(arr);
 });
 
+
+/**
+	Return current sensor data 
+*/
+app.get("/sensor/data", (req, res, next) => {
+	res.jsonp(memStorage.sensorData);
+
+});
+
+/**
+	Return a path to the latest camera image 
+*/
+app.get("/cam/image", (req, res, next) => {
+	res.jsonp(memStorage.cameraImgPath);
+});
+
+
+
 /**
 	Motion detected
 */
 app.post("/motion", (req, res, next) => {
  	console.log(req.body);
+ 	memStorage.isMotionDetected = true;
  	res.send("New motion, got it");
 });
 
 /**
 	Camera sent an image url
 */
-app.post("/cam/images", (req, res, next) => {
+app.post("/cam/image", (req, res, next) => {
  	console.log(req.body);
+ 	memStorage.cameraImgPath = req.body["imgPath"];
  	res.send("New image, got it");
 });
